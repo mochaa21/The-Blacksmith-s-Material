c787972e275ffa74eb9cb65340e15027ef188900
mochaa21 <mochamadsyahid573@gmail.com>
2026-09-09 11:02:50 +0700

    feat: first commit

---
+++ main.py
@@ -0,0 +1,20 @@
+from typing import List
+
+def cari_material_tempa(material: List[int], target: int) -> List[int]:
+    # 1. Siapkan laci/buku catatan kosong lu di sini
+    
+    # 2. Lakukan looping dengan enumerate
+    
+    # 3. Hitung selisih dari target dan angka saat ini
+    
+    # 4. Cek apakah selisih tersebut sudah ada di laci. 
+    #    Jika ada, return [indeks_dari_laci, indeks_saat_ini].
+    
+    # 5. Jika tidak ada, masukkan angka saat ini sebagai nama laci (Key), 
+    #    dan indeksnya sebagai isi laci (Value).
+    pass
+
+# --- EKSEKUSI ---
+tas_material = [4, 9, 11, 6]
+target_elemen = 15
+print(cari_material_tempa(tas_material, target_elemen))
